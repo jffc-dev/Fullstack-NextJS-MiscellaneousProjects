1a3aa7fc71893a0a7771269dca186392eba10426
Javier Flores <jffc.dev@gmail.com>
2023-04-11 23:51:57 -0500

    fix(App): spell check applied

--- 00-first-app/components/layout.js
+++ 00-first-app/components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Javier Flores Cardenas';
+const name = 'Javier Flores Cárdenas';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
